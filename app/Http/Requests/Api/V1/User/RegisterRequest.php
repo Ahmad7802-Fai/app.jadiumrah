d6a453ef06dfc22317c13e37e16fbc35ca34dff5
Ahmad Faizi Kamil <jadiumrah.jakarta@gmail.com>
2026-03-31 10:14:43 +0700

    fix sactum

--- app/Http/Requests/Api/V1/User/RegisterRequest.php
+++ app/Http/Requests/Api/V1/User/RegisterRequest.php
@@ -3,12 +3,14 @@
 namespace App\Http\Requests\Api\V1\User;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class RegisterRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; // 🔥 WAJIB
+        return true;
     }
 
     public function rules(): array
@@ -19,22 +21,13 @@ class RegisterRequest extends FormRequest
             'password' => ['required','confirmed','min:6'],
         ];
     }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation error',
+            'errors' => $validator->errors()
+        ], 422));
+    }
 }
-
-// class RegisterRequest extends FormRequest
-// {
-//     public function authorize(): bool
-//     {
-//         return true;
-//     }
-
-//     public function rules(): array
-//     {
-//         return [
-//             'name' => ['required','string','max:255'],
-//             'email' => ['required','email','unique:users,email'],
-//             'password' => ['required','min:6','confirmed'],
-//             'phone' => ['nullable','string','max:20'],
-//         ];
-//     }
-// }
